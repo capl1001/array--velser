07e9c24893a1d33189bac11322afdf4245278701
Caroline Plahter <capl1001@stud.ek.dk>
2026-09-22 15:11:41 +0200

    min færdige butik

---
+++ script.js
@@ -0,0 +1,33 @@
+// console.log("Test");
+
+// const undervisere = ["Anders", "Alan", "Stine"];
+// console.log(undervisere);
+
+// const section = document.querySelector("section");
+
+// undervisere.forEach(visNavne);
+
+// function visNavne(elm) {
+//   section.innerHTML += `<p>${elm}</p>`;
+// }
+const endpoint = "https://kea-alt-del.dk/t7/api/products?limit=20";
+
+const produktliste = document.querySelector("section");
+
+fetch(endpoint)
+  .then((res) => res.json())
+  .then(visData);
+
+function visData(json) {
+  console.log(json);
+  json.forEach((element) => {
+    produktliste.innerHTML += `
+    <article class="card">
+    <img src=https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp alt="produktbillede" />
+  <h2>${element.productdisplayname}</h2>
+  <h3>${element.brandname}</h3>
+  <p>${element.price}</p>
+  <p>${element.subcategory}</p>
+  </article>`;
+  });
+}
